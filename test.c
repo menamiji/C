db296216be29f91384d5ac2c5d652f4e76ff3f40
menamiji <menamiji@gmail.com>
2021-12-21 20:54:34 +0900

    stack

--- test.c
+++ test.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+    struct node * link;
+};
+typedef struct node Node;
+
+void printNode(Node * top){
+    printf("[bottom]");
+    while(top!=NULL) {
+        printf("%d", top->value);
+        top=top->link;
+    }
+    printf("[top]\n");
+}
+
+Node * push(Node * top, int num){
+    Node * temp = (Node *)malloc(sizeof(Node));
+    temp->value = num;
+    temp->link = top;
+    return temp;
+}
+
+Node * pop(Node * top){
+    Node * nextLink=top->link;
+    printf("pop : %d\n", top->value);
+    free(top);
+    return nextLink;  
+}
+
 int main(void) {
-    printf("aaabcccbbba");
+    Node * top = NULL;
+    int num;
+    
+    while(1) {
+        printf("입력값 : ");
+        scanf("%d", &num);
+        if(num!=0) {
+            top = push(top, num);
+            printNode(top);
+        }
+        else {
+            top = pop(top);
+            printNode(top);
+        }
+    }
     return 0;
 }
 
